1117a848fb565a61635bcbc4f31f9ff54bc182d9
Tom Workman <tworkman512@gmail.com>
2019-04-08 16:18:16 -0500

    test out the server

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -20,21 +20,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: ``,
-  secret: `mysecret123`
+  endpoint: `https://us1.prisma.sh/tom-workman-e09d7f/hackernews-node/dev`
 });
 exports.prisma = new exports.Prisma();
-var models = [
-  {
-    name: "Link",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Vote",
-    embedded: false
-  }
-];
