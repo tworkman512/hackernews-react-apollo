2d2ebc40ef51f0011b7b8c7cf30c46f2622e5045
Tom Workman <tworkman512@gmail.com>
2019-04-10 13:50:02 -0500

    Adds Mutation component and gql parser function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { AUTH_TOKEN } from '../constants'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
 export default class Login extends Component {
   state = {
@@ -11,6 +13,22 @@ export default class Login extends Component {
 
   render() {
     const { login, email, password, name } = this.state
+
+    const SIGNUP_MUTATION = gql`
+      mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+        signup(email: $email, password: $password, name: $name) {
+          token
+        }
+      }
+    `
+
+    const LOGIN_MUTATION = gql`
+      mutation LoginMutation($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          token
+        }
+      }
+    `
     return (
       <div>
         <h4 className="mv3">{login ? 'Login' : 'Sign Up'}</h4>
@@ -37,16 +55,22 @@ export default class Login extends Component {
           />
         </div>
         <div className="flex mt3">
-          <div className="pointer mr2 button" onClick={() => this._confirm()}>
-            {login ? 'login' : 'create account'}
-          </div>
+          <Mutation
+            mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+            variables={{ email, password, name }}
+            onCompleted={data => this._confirm(data)}
+          >
+            {mutation => (
+              <div className="pointer mr2 button" onClick={mutation}>
+                {login ? 'login' : 'create account'}
+              </div>
+            )}
+          </Mutation>
           <div
             className="pointer button"
             onClick={() => this.setState({ login: !login })}
           >
-            {login
-              ? 'need to create an account?'
-              : 'already have an account?'}
+            {login ? 'need to create an account?' : 'already have an account?'}
           </div>
         </div>
       </div>
