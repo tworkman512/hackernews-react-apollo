2e0abf63f3a49dd0764d1830dbea860b65b4dfb3
Tom Workman <tworkman512@gmail.com>
2019-04-11 16:31:27 -0500

    adds link votes ui

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -13,6 +13,16 @@ export default class LinkList extends Component {
             createdAt
             url
             description
+            postedBy {
+              id
+              name
+            }
+            votes {
+              id
+              user {
+                id
+              }
+            }
           }
         }
       }
@@ -39,7 +49,9 @@ export default class LinkList extends Component {
 
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link, index) => (
+                <Link key={link.id} link={link} index={index} />
+              ))}
             </div>
           )
         }}
